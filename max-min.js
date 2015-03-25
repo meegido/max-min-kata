f0632631857036e83e26d85505a278f3c0c13cbe
Mercedes Egido <meegido@gmail.com>
2015-03-25 12:17:38 +0100

    first try

---
+++ max-min.js
@@ -0,0 +1,41 @@
+
+
+function min_element (sequence) {
+	var maximumNumber =  1000;
+
+	for (i = 0; i < sequence.length; i++){
+
+		if(sequence.length < maximumNumber){
+			return sequence.Math.min.applay(sequence)
+		}
+	}
+}
+
+function max_element (sequence) {
+
+	
+	return 99
+}
+
+
+
+console.log(min_element([12,4,99,7]));
+console.log(max_element([12,4,99,7]));
+
+
+
+
+
+
+/*Escribir el par de funciones
+
+min_element(S) y
+max_element(S)
+
+La primera debe obtener el mínimo de una secuencia de elementos.
+La segunda debe obtener el máximo de una secuencia de elementos.
+
+Ejemplo:
+min_element([12,4,99,7]) debe devolver 4
+max_element([12,4,99,7]) debe devolver 99
+*/
